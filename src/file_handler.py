444cc76179ac7086c83ec1781c939185d75798da
NilsDy <n.dycke@gmx.de>
2018-05-11 16:34:28 +0200

    added ignore

--- src/file_handler.py
+++ src/file_handler.py
@@ -16,8 +16,8 @@ def load_file(file_name):
 		if(elements[0] == ''):
 			continue;
 		if(len(elements) >= 3):
-			elements_structured = (elements[1].encode('utf8'), elements[2].encode('utf8'))
+			elements_structured = (unicode(elements[1], 'utf8'), unicode(elements[2],'utf8'))
 		else:
-			elements_structured = (elements[1].encode('utf8'))
+			elements_structured = (unicode(elements[1], 'utf8'))
 		loaded_lines[elements[0]] = elements_structured;
 	return loaded_lines;
